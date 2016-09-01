cdc39a364fe0e2c3c13da5ac3606a1c4d756f833
jannakha <j_malikova@yahoo.com>
2016-09-01 18:01:07 +1000

    updated layout

--- elements/uqlibrary-training-details.js
+++ elements/uqlibrary-training-details.js
@@ -9,18 +9,36 @@
         type: Object,
         observer: '_eventChanged'
       },
+
+      //TODO: pass user account from parent
+      userAccount: {
+        type: Object,
+        value: function() {
+          return { hasSession: true };
+        }
+      },
+
+      showLoginButton: {
+        type: Object,
+        value: false
+      },
+
       gaAppName: {
         type: String
       },
+
       _startTime: {
         type: String
       },
+
       _endTime: {
         type: String
       },
+
       _fullDate: {
         type: String
       },
+
       _bookingText: {
         type: String
       }
@@ -41,19 +59,10 @@
       if (this.event.bookingSettings !== null) {
         this._bookingText = 'Places sill available';
 
-        console.log(this.event.bookingSettings.bookingLimit);
-        console.log(this.event.attendance.total);
-
         if (this.event.bookingSettings.placesRemaining == 0) {
             this._bookingText = 'Class is full. Register for waitlist.';
           }
       }
-    },
-    /**
-     * Called when the user presses the close/back button
-     */
-    _showListView: function () {
-      this.fire('show-list');
     },
     /**
      * Open event
@@ -61,6 +70,35 @@
     _openEvent: function () {
       this.$.ga.addEvent('Library Training link clicked', this.event.name);
       window.open(this.event.link);
+    },
+
+    sendEmail: function(e) {
+
+      var trainingEvent = this.event;
+
+      if (this.userAccount && this.userAccount.hasSession) {
+        //user is logged in, create email template
+        var mailText = 'mailto:training_events@library.uq.edu.au?';
+        mailText += '&subject=Expression of interest for event';
+        mailText += '&body=Hi, \nI\'d like to participate in the following training event: \n\n';
+        mailText += 'Event Id: ' + trainingEvent.entityId + '\n';
+        mailText += 'Event Title: ' + trainingEvent.name + '\n';
+        mailText += 'Event Date: ' + this._fullDate + ' at ' + this._startTime + ' (' + trainingEvent.start + ')' + '\n';
+        mailText += 'Username: ' + this.userAccount.id + '\n';
+        mailText += 'Name: ' + this.userAccount.name + '\n';
+        mailText += 'Email: ' + this.userAccount.mail + '\n';
+        mailText += '\n\nThank you' + ',\n' + this.userAccount.name;
+
+        window.location = encodeURI(mailText);
+
+      } else {
+        //user is not logged in, redirect to login page
+        var href = window.location.href;
+        if (href.indexOf('?') > 0)
+          href = href.substr(0, href.indexOf('?'));
+        this.$.accountApi.login(href + '?eventId=' + trainingEvent.entityId + '#' + trainingEvent.entityId);
+      }
+
     }
   });
 })();
