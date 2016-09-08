6ca2b3f5cacd71c400815b29dd238fc0aa6d060a
jannakha <j_malikova@yahoo.com>
2016-09-08 18:21:27 +1000

    added category component

---
+++ elements/uqlibrary-training-list-category.js
@@ -0,0 +1,225 @@
+(function () {
+  Polymer({
+    is: 'uqlibrary-training-list-category',
+    properties: {
+      gaAppName: {
+        type: String
+      },
+      filter: {
+        type: Object,
+        value: function () {
+          return {
+            keyword: '',
+            month: '',
+            campus: 'Herston'
+          };
+        }
+      },
+      category: {
+        type: Object,
+        value: function() {
+          return {
+            displayName : "Endnote",
+            events: [
+              {
+                "entityId": 464165,
+                "created": "2016-07-08T16:33:16.6+10:00",
+                "updated": "2016-07-08T16:33:16.6+10:00",
+                "name": "How to Write a Successful Resume and Cover Letter!",
+                "start": "2016-07-15T12:00:00+10:00",
+                "formattedDate" : "1 Sep 2016",
+                "end": "2016-07-15T14:00:00+10:00",
+                "publish": "2016-07-08T00:00:00+10:00",
+                "expire": "2016-07-15T14:00:00+10:00",
+                "summary": "Your resume/CV and cover letter create a first impression, so you want them to be noticed by employers. Is your application at the professional standard expected by recruiters for internships and grad programs?\r\nCome along to this session and find out How to write a strong Resume and Cover Letter!",
+                "details": "<p>Your resume is your most important marketing tool. Learn how to market yourself effectively and get ahead of the crowd! </p><p>This 2 hour workshop will help you to prepare a professional resume and tailor your cover letter&nbsp;to reflect your skills, knowledge, education and achievements relevant to the job you are applying for.</p><p>This workshop will cover information on:</p><ol><li>Analysing a job ad&nbsp;&amp; understanding what an employer wants</li><li>What to include and how to format your&nbsp;resume</li><li>The importance of a Cover Letter</li><li>Cover letter Format and Content</li></ol><p>This workshop requires you to bring your current resume. It doesn't matter whether it is your high school resume or a more recent one for a casual job.</p><p>This workshop is accessible to all UQ Students and Alumni of up to 6 months. Bookings are not required but please make sure to arrive on time to secure your seat. If we have reached full capacity you will be directed to the next available workshop.</p><p>*During the first 8 weeks of Semester it is advised you arrive at least 15 prior to start time.&nbsp;</p>",
+                "venue": "Student Services, level 2, Building 21C, Media Room 215\r\n2 hours",
+                "bookingType": 0,
+                "bookingSettings": {
+                  "bookingLimit": 40,
+                  "placesRemaining": 0,
+                  "bookingsOpen": null,
+                  "bookingsClose": "2016-07-22T08:30:00+10:00"
+                },
+                "workGroupId": null,
+                "providers": {
+                  "organisations": [],
+                  "administrators": [
+                    {
+                      "id": 118568,
+                      "firstName": "Nicky",
+                      "lastName": "Spijkerman",
+                      "email": "n.spijkerman@uq.edu.au"
+                    }
+                  ]
+                },
+                "attendance": {
+                  "total": 40,
+                  "unspecified": 0,
+                  "absent": 0,
+                  "attended": 0
+                },
+                "categories": {
+                  "eventType": "Career and Job Assistance",
+                  "campus": [
+                    "Herston"
+                  ]
+                }
+              },
+              {
+                "entityId": 464166,
+                "created": "2016-07-08T16:37:44.5+10:00",
+                "updated": "2016-07-08T16:37:44.5+10:00",
+                "name": " Interview Excellence: The steps to interview success",
+                "start": "2016-07-19T12:00:00+10:00",
+                "end": "2016-07-19T14:00:00+10:00",
+                "publish": "2016-07-08T00:00:00+10:00",
+                "expire": "2016-07-19T14:00:00+10:00",
+                "summary": "Want to improve you interview skills? The workshop provides valuable information to help you nail your next interview and gives solid guidance on how to best prepare. Gain an insight into employer expectations while developing confidence in your unique skills and achievements.",
+                "details": "<p>You have landed an interview and now the only thing between you and the job is how you sell yourself on the day!</p><p>This workshop will teach you how to prepare and present yourself at interview and increase your chances of securing the job.</p><p>This workshop will cover information on:</p><ul>\r\n<li>Researching for the role and how to better prepare yourself</li><li>General and behavioural interview questions/responses</li><li>How to handle the curly questions</li><li>Selling your skills and attributes to leave a positive lasting impression</li></ul>",
+                "venue": "Media Room 215\r\nStudent Union Complex, Bld 21C (directly under the Pizza Cafe)",
+                "bookingType": 0,
+                "bookingSettings": null,
+                "workGroupId": null,
+                "providers": {
+                  "organisations": [],
+                  "administrators": [
+                    {
+                      "id": 198164,
+                      "firstName": "Lawrence",
+                      "lastName": "Casey",
+                      "email": "l.casey2@uq.edu.au"
+                    }
+                  ]
+                },
+                "attendance": {
+                  "total": 0,
+                  "unspecified": 0,
+                  "absent": 0,
+                  "attended": 0
+                },
+                "categories": {
+                  "eventType": "Career and Job Assistance",
+                  "campus": [
+                    "St Lucia"
+                  ]
+                }
+              },
+              {
+                "entityId": 442519,
+                "created": "2016-06-24T08:03:47.597+10:00",
+                "updated": "2016-07-08T14:47:53.917+10:00",
+                "name": "UQ 2016 Student Summit - Think Tank Session",
+                "start": "2016-07-21T08:30:00+10:00",
+                "end": "2016-07-21T10:30:00+10:00",
+                "publish": "2016-06-24T00:00:00+10:00",
+                "expire": "2016-07-21T10:30:00+10:00",
+                "summary": "This interactive workshop will help you to prepare for the Summit, by working in groups to summarise the key issues for discussion at the event.\r\n\r\nThis event is only available to successful candidates for the UQ 2016 Student Summit program.",
+                "details": "<p>This interactive workshop will help you to prepare for the Summit, by working in groups to summarise the key issues for discussion at the event.</p><p>This event is only available to successful candidates for the UQ 2016 Student Summit program.</p>",
+                "venue": "Room 273- Global Change Institute Building # 20",
+                "bookingType": 1,
+                "bookingSettings": {
+                  "bookingLimit": 40,
+                  "placesRemaining": 9,
+                  "bookingsOpen": null,
+                  "bookingsClose": "2016-07-21T08:30:00+10:00"
+                },
+                "workGroupId": null,
+                "providers": {
+                  "organisations": [],
+                  "administrators": [
+                    {
+                      "id": 170688,
+                      "firstName": "Belinda",
+                      "lastName": "Brear",
+                      "email": "b.brear@uq.edu.au"
+                    }
+                  ]
+                },
+                "attendance": {
+                  "total": 31,
+                  "unspecified": 31,
+                  "absent": 0,
+                  "attended": 0
+                },
+                "categories": {
+                  "eventType": "Undergraduate Student Events",
+                  "campus": [
+                    "St Lucia"
+                  ]
+                }
+              },
+              {
+                "entityId": 464167,
+                "created": "2016-07-08T16:41:30.353+10:00",
+                "updated": "2016-07-08T16:41:30.353+10:00",
+                "name": "How to Write a Successful Resume and Cover Letter!",
+                "start": "2016-07-21T13:00:00+10:00",
+                "end": "2016-07-21T15:00:00+10:00",
+                "publish": "2016-07-08T00:00:00+10:00",
+                "expire": "2016-07-21T15:00:00+10:00",
+                "summary": "Your resume/CV and cover letter create a first impression, so you want them to be noticed by employers. Is your application at the professional standard expected by recruiters for internships and grad programs?\r\nCome along to this session and find out How to write a strong Resume and Cover Letter!",
+                "details": "<p>Your resume is your most important marketing tool. Learn how to market yourself effectively and get ahead of the crowd! </p><p>This 2 hour workshop will help you to prepare a professional resume and tailor your cover letter&nbsp;to reflect your skills, knowledge, education and achievements relevant to the job you are applying for.</p><p>This workshop will cover information on:</p><ol>\r\n<li>Analysing a job ad&nbsp;&amp; understanding what an employer wants</li><li>What to include and how to format your&nbsp;resume</li><li>The importance of a Cover Letter</li><li>Cover letter Format and Content</li></ol><p>This workshop requires you to bring your current resume. It doesn't matter whether it is your high school resume or a more recent one for a casual job.</p><p>This workshop is accessible to all UQ Students and Alumni of up to 6 months. Bookings are not required but please make sure to arrive on time to secure your seat. If we have reached full capacity you will be directed to the next available workshop.</p><p>*During the first 8 weeks of Semester it is advised you arrive at least 15 prior to start time.&nbsp;</p>",
+                "venue": "Student Services, level 2, Building 21C, Media Room 215\r\n2 hours",
+                "bookingType": 0,
+                "bookingSettings": null,
+                "workGroupId": null,
+                "providers": {
+                  "organisations": [],
+                  "administrators": [
+                    {
+                      "id": 212668,
+                      "firstName": "Paige",
+                      "lastName": "McDonough",
+                      "email": "p.mcdonough@uq.edu.au"
+                    }
+                  ]
+                },
+                "attendance": {
+                  "total": 0,
+                  "unspecified": 0,
+                  "absent": 0,
+                  "attended": 0
+                },
+                "categories": {
+                  "eventType": "Career and Job Assistance",
+                  "campus": [
+                    "St Lucia"
+                  ]
+                }
+              }
+            ]
+          };
+        }
+      }
+    },
+
+    fitsFilter: function() {
+
+      return function(trainingEvent) {
+        // console.log(trainingEvent);
+        return false;
+      };
+
+    },
+
+    toggle: function(event) {
+
+      console.log(event.model);
+
+      var toggleId = 'collapse' + event.model.trainingEvent.entityId;
+
+      //close all other toggles
+      var openedItems = this.querySelectorAll('.iron-collapse-opened');
+      for(var index = 0; index < openedItems.length; index++) {
+        var item = openedItems[index];
+        //do not toggle current item
+        if (item.id.indexOf(toggleId) < 0)
+          item.toggle();
+      }
+
+      this.querySelector('#' + toggleId).toggle();
+    }
+
+  });
+})();
