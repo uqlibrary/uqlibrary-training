dc2c4c0f10f614ebd1f000af3e36c8734abf41ed
Lea de Groot <l.degroot@library.uq.edu.au>
2018-09-19 17:00:19 +1000

    fiddle with the mapping call [PT #158897400]

--- elements/uqlibrary-training-details.js
+++ elements/uqlibrary-training-details.js
@@ -111,32 +111,33 @@
         // latlong: latitude & longitude for the map url
 
         var listKnownLocations = [
-            { "locationHint": "Biological Sciences Library", "display": "Biological+Sciences+Library", "latlong": "-27.4969967,153.0113495", "zoom": 20, "query_place_id": "ChIJbaJ6sYNQkWsRYvptjIAIPRE" },
-            { "locationHint": "Colin Clark B", "display": "Colin+Clark+Building", "latlong": "-27.4947559,153.0140859", "zoom": 20 },
-            { "locationHint": "Duhig Tower", "display": "Duhig+Tower", "latlong": "-27.4966319,153.0144148", "zoom": 20 },
-            { "locationHint": "Duhig B", "display": "Duhig+Tower", "latlong": "-27.4966319,153.0144148", "zoom": 20 },
-            { "locationHint": "Forgan Smith B", "display": "Forgan+Smith+Building,+St+Lucia+QLD+4072", "latlong": "-27.496937,153.0128046", "zoom": 20 },
-            { "locationHint": "Hawken B", "display": "Dorothy+Hill+Engineering+and+Sciences+Library", "latlong": "-27.5000086,153.0132045", "zoom": 20 },
-            { "locationHint": "Sir Llew Edwards B", "display": "Sir+Llew+Edwards+Building", "latlong": "-27.4957145,153.0132919", "zoom": 20 },
-            { "locationHint": "Zelman Cowan B", "display": "Zelman+Cowen+Building,+St+Lucia+QLD+4067", "latlong": "-27.4990138,153.0144133", "zoom": 20 },
-            { "locationHint": "Gatton Library", "display": "UQ+Gatton+J.K.+Murray+Library", "latlong": "-27.5550314,152.3357461", "zoom": 20 },
-            { "locationHint": "Herston B", "display": "Herston+Health+Sciences+Library", "latlong": "-27.4488643,153.0277196", "zoom": 20 },
-            { "locationHint": "School of Public Health B", "display": "School+of+Public+Health", "latlong": "-27.4487423,153.0231839", "zoom": 20 },
-            { "locationHint": "UQCCR Building", "display": "UQCCR", "latlong": "-27.4487067,153.028542", "zoom": 20 },
-            { "locationHint": "Aubigny Place B", "display": "Aubigny+Place+-+Mater+Hospital+Brisbane", "latlong": "-27.486558,153.027563", "zoom": 20, "placeID": 'ChIJ586ySG1akWsRwlxYji6m8kw' },
-            { "locationHint": "PACE Health Sciences Library", "display": "UQ+School+Of+Public+Health", "latlong": "-27.4999636,153.0303293", "zoom": 19 },
-            { "locationHint": "Bundaberg", "display": "UQ+Health+Sciences+Learning+%26+Discovery+Centre", "latlong": "-24.8688817,152.3321844", "zoom": 18 },
-            { "locationHint": "Hervey Bay", "display": "UQ+Health+Sciences+Learning+%26+Discovery+Centre", "latlong": "-25.298911,152.8212355", "zoom": 20 },
+            { "locationHint": "Biological Sciences Library", "display": "Biological%20Sciences%20Library%2C%20The%20University%20of%20Queensland%2C%20Slip%20Road%2C%20Saint%20Lucia%20QLD%2C%20Australia", "latlong": "-27.4969967,153.0113495", "zoom": 20, "query_place_id": "ChIJbaJ6sYNQkWsRYvptjIAIPRE" },
+            { "locationHint": "Colin Clark B", "display": "Colin%20Clark%20Building%2C%20University%20Drive%2C%20Saint%20Lucia%20QLD%2C%20Australia", "latlong": "-27.4947559,153.0140859", "zoom": 20 },
+            { "locationHint": "Duhig Tower", "display": "Duhig%20Tower%2C%20Sir%20William%20MacGregor%20Drive%2C%20Saint%20Lucia%20QLD%2C%20Australia", "latlong": "-27.4966319,153.0144148", "zoom": 20 },
+            { "locationHint": "Duhig B", "display": "Duhig%20Tower%2C%20Sir%20William%20MacGregor%20Drive%2C%20Saint%20Lucia%20QLD%2C%20Australia", "latlong": "-27.4966319,153.0144148", "zoom": 20 },
+            { "locationHint": "Forgan Smith B", "display": "Forgan%20Smith%20Building%2C%20Saint%20Lucia%20QLD%2C%20Australia", "latlong": "-27.496937,153.0128046", "zoom": 20 },
+            { "locationHint": "Hawken B", "display": "Dorothy%20Hill%20Engineering%20and%20Sciences%20Library%2C%20Cooper%20Road%2C%20Saint%20Lucia%20QLD%2C%20Australia", "latlong": "-27.5000086,153.0132045", "zoom": 20 },
+            { "locationHint": "Sir Llew Edwards B", "display": "Sir%20Llew%20Edwards%20Building%2C%20Saint%20Lucia%20QLD%2C%20Australia", "latlong": "-27.4957145,153.0132919", "zoom": 20 },
+            { "locationHint": "Zelman Cowan B", "display": "Zelman%20Cowen%20Building%20%2851%23%29%20UQ%20St%20Lucia%20Campus%2C%20Saint%20Lucia%20QLD%2C%20Australia", "latlong": "-27.4990138,153.0144133", "zoom": 20 },
+            { "locationHint": "Gatton Library", "display": "-27.5550314,152.3357461", "latlong": "-27.5550314,152.3357461", "zoom": 20 }, // UQ+Gatton+J.K.+Murray+Library
+            { "locationHint": "Herston B", "display": "Herston%20Health%20Sciences%20Library%2C%20Herston%20Road%2C%20Brisbane%20City%20QLD%2C%20Australia", "latlong": "-27.4488643,153.0277196", "zoom": 20 },
+            { "locationHint": "School of Public Health B", "display": "UQ%20School%20Of%20Public%20Health%2C%20Herston%20Road%2C%20Herston%20QLD%2C%20Australia", "latlong": "-27.4487423,153.0231839", "zoom": 20 },
+            { "locationHint": "UQCCR Building", "display": "UQCCR%2C%20Building%20B71%2C%20Brisbane%20City%20QLD%2C%20Australia", "latlong": "-27.4487067,153.028542", "zoom": 20 },
+            { "locationHint": "Aubigny Place B", "display": "Aubigny%20Place%20-%20Mater%20Hospital%20Brisbane%2C%20Annerley%20Road%2C%20South%20Brisbane%20QLD%2C%20Australia", "latlong": "-27.486558,153.027563", "zoom": 20, "placeID": 'ChIJ586ySG1akWsRwlxYji6m8kw' },
+            { "locationHint": "PACE Health Sciences Library", "display": "PACE%20Health%20Sciences%20Library%2C%20Cornwall%20Street%2C%20Woolloongabba%20QLD%2C%20Australia", "latlong": "-27.4999636,153.0303293", "zoom": 19 },
+            { "locationHint": "Bundaberg", "display": "UQ%20Health%20Sciences%20Learning%20%26%20Discovery%20Centre%2C%20Bundaberg%20West%20QLD%2C%20Australia", "latlong": "-24.8688817,152.3321844", "zoom": 18 },
+            { "locationHint": "Hervey Bay", "display": "-25.298911,152.8212355", "latlong": "-25.298911,152.8212355", "zoom": 20 }, // UQ+Health+Sciences+Learning+%26+Discovery+Centre
             { "locationHint": "Rockhampton", "display": "The+University+of+Queensland,+Rural+Clinical+School,+Rockhampton", "latlong": "-23.3809301,150.496215", "zoom": 18 },
-            { "locationHint": "Toowoomba", "display": "School's+Teaching+and+Learning+Centre+South+Toowoomba", "latlong": "-27.568576,151.9421979", "zoom": 20 }
+            { "locationHint": "Toowoomba", "display": "Toowoomba%20Rural%20Clinical%20School%2C%20152%20West%20Street%2C%20South%20Toowoomba%20QLD%2C%20Australia", "latlong": "-27.568576,151.9421979", "zoom": 20 }
         ];
+
         var url = false;
         for (var i = 0; i < listKnownLocations.length; i++) {
             var trainRegExp = new RegExp(listKnownLocations[i].locationHint, 'i');
             if (venue.match(trainRegExp)) {
               // after much experimentation, this format currently works on desktop AND ios AND android!
               // maps docs: https://developers.google.com/maps/documentation/urls/guide
-              url = 'https://www.google.com/maps/search/?api=1&query=' + listKnownLocations[i].latlong;
+              url = 'https://www.google.com/maps/search/?api=1&query=' + listKnownLocations[i].display;
               break;
             }
         }
